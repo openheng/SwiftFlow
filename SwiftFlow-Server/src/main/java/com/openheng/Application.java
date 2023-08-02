2db65bc6a72721bf560745c094382fa7f5c5d68c
openheng <opensource78523@outlook.com>
2023-08-02 15:42:59 +0800

    add pom and Module

---
+++ SwiftFlow-Server/src/main/java/com/openheng/Application.java
@@ -0,0 +1,17 @@
+package com.openheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by openheng on 2023-08-02.
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

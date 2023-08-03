2ce381f6050e8b61f4f89f29f72a01f893cfec75
openheng <opensource78523@outlook.com>
2023-08-03 20:28:59 +0800

    add Mybatis generator file

---
+++ SwiftFlow-Server/src/test/java/DataBaseTest.java
@@ -0,0 +1,27 @@
+import com.openheng.Application;
+import com.openheng.dao.TaskFlow;
+import com.openheng.dao.TaskFlowMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by openheng on 2023-08-03.
+ */
+@SpringBootTest(classes = Application.class)
+public class DataBaseTest {
+
+    @Autowired
+    private TaskFlowMapper taskFlowMapper;
+
+    @Test
+    public void testInsert() {
+        TaskFlow mockTaskFlow = new TaskFlow();
+        mockTaskFlow.setId(System.currentTimeMillis());
+        mockTaskFlow.setBiz_code((byte) 0);
+        mockTaskFlow.setName("mock");
+        int insertNum = taskFlowMapper.insertSelective(mockTaskFlow);
+        Assertions.assertNotEquals(insertNum, 0);
+    }
+}
